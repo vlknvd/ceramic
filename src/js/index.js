b9f4be74813994480a71919b038b4c71d892d379
darya <vlknvd@yandex.ru>
2025-09-02 22:04:57 +0300

    Добавлен base для GitHub Pages

--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,41 @@
-import Swiper from "swiper";
-import { Navigation, Pagination } from "swiper/modules";
-import JustValidate from "just-validate";
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import JustValidate from 'just-validate';
 
-import "purecss/build/grids-min.css";
-import "purecss/build/grids-responsive-min.css";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "../sass/style.scss";
+import 'purecss/build/grids-min.css';
+import 'purecss/build/grids-responsive-min.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import '../sass/style.scss';
 
-const menu = document.querySelector(".header__menu");
+const menu = document.querySelector('.header__menu');
 
-document.querySelector(".burger").addEventListener("click", (e) => {
-  menu.classList.add("header__menu-active");
-  document.body.style.overflow = "hidden";
+document.querySelector('.burger').addEventListener('click', (e) => {
+  menu.classList.add('header__menu-active');
+  document.body.style.overflow = 'hidden';
 });
 
-document.querySelector(".header__menu-close").addEventListener("click", () => {
-  menu.classList.remove("header__menu-active");
-  document.body.style.overflow = "";
+document.querySelector('.header__menu-close').addEventListener('click', () => {
+  menu.classList.remove('header__menu-active');
+  document.body.style.overflow = '';
 });
 
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
-  //сколько слайдов будет показываться на самой первой версии(mobile first)
   loop: true,
-  // зацикливание слайдера
   pagination: {
-    // пагинация (точки для переключения слайдов)
-    el: ".swiper-pagination",
-    // какой элемент будет отвечать за переключение
+    el: '.swiper-pagination',
     clickable: true,
   },
   navigation: {
-    // навигация (переключение стрелками)
-    nextEl: ".icon-right",
-    // кнопка отвечающая за переключение вперед
-    prevEl: ".icon-left",
-    // кнопка отвечающая за переключение назад
+    nextEl: '.icon-right',
+    prevEl: '.icon-left',
   },
   breakpoints: {
-    // брейкпоинты
     1200: {
       slidesPerView: 3,
-      //   сколько слайдов показывать когда экран > 1120
       spaceBetween: 5,
-      //   какое расстояние между слайдами
     },
     1920: {
       spaceBetween: 35,
@@ -54,30 +44,30 @@ const swiper = new Swiper(".swiper", {
   modules: [Navigation, Pagination],
 });
 
-const tabs = document.querySelectorAll(".catalog__tab"),
-  content = document.querySelectorAll(".catalog__content-item");
+const tabs = document.querySelectorAll('.catalog__tab'),
+  content = document.querySelectorAll('.catalog__content-item');
 
 tabs.forEach((tab, i) => {
-  tab.addEventListener("click", () => {
-    tabs.forEach((t) => t.classList.remove("catalog__tab-active"));
-    content.forEach((c) => (c.style.display = "none"));
+  tab.addEventListener('click', () => {
+    tabs.forEach((t) => t.classList.remove('catalog__tab-active'));
+    content.forEach((c) => (c.style.display = 'none'));
 
-    tab.classList.add("catalog__tab-active");
-    content[i].style.display = "flex";
+    tab.classList.add('catalog__tab-active');
+    content[i].style.display = 'flex';
   });
 });
 
-content.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
+content.forEach((c, i) => (c.style.display = i === 0 ? 'flex' : 'none'));
 
 const totalData = {
   jackets: 5,
-  hats: "empty",
-  socks: "empty",
+  hats: 'empty',
+  socks: 'empty',
   pants: 15,
   scissors: 15,
   paper: true,
   dishwashers: 3,
-  cookers: "empty",
+  cookers: 'empty',
   mixers: 14,
   deficit: true,
   date: new Date(),
@@ -85,103 +75,99 @@ const totalData = {
 
 function printReport(data) {
   let arr = Object.entries(data)
-    .filter((data) => data.includes("empty"))
+    .filter((data) => data.includes('empty'))
     .map((val) => val[0])
-    .join(", ");
+    .join(', ');
   console.log(`We need this items: ${arr}`);
-
-  // return `We need this items: ${"..."}`;
-  // // или
-  // return "Everything fine";
 }
 
 printReport(totalData);
 
-const validator = new JustValidate(".touch__form", {
+const validator = new JustValidate('.touch__form', {
   submitFormAutomatically: true,
 });
 validator
-  .addField(document.querySelector("#name"), [
+  .addField(document.querySelector('#name'), [
     {
-      rule: "required",
-      errorMessage: "Please fill the name",
+      rule: 'required',
+      errorMessage: 'Please fill the name',
     },
     {
-      rule: "minLength",
+      rule: 'minLength',
       value: 2,
-      errorMessage: "Min 2 char",
+      errorMessage: 'Min 2 char',
     },
   ])
-  .addField(document.querySelector("#email"), [
+  .addField(document.querySelector('#email'), [
     {
-      rule: "required",
+      rule: 'required',
     },
     {
-      rule: "email",
+      rule: 'email',
     },
   ])
   .addField(
-    document.querySelector("#question"),
+    document.querySelector('#question'),
     [
       {
-        rule: "required",
+        rule: 'required',
       },
       {
-        rule: "minLength",
+        rule: 'minLength',
         value: 5,
       },
     ],
     {
       errorsContainer: document
-        .querySelector("#question")
-        .parentElement.querySelector(".error-message"),
+        .querySelector('#question')
+        .parentElement.querySelector('.error-message'),
     }
   )
   .addField(
-    document.querySelector("#checkbox"),
+    document.querySelector('#checkbox'),
     [
       {
-        rule: "required",
+        rule: 'required',
       },
     ],
     {
       errorsContainer: document
-        .querySelector("#checkbox")
-        .parentElement.parentElement.querySelector(".checkbox-error-message"),
+        .querySelector('#checkbox')
+        .parentElement.parentElement.querySelector('.checkbox-error-message'),
     }
   );
 
-const footerValidator = new JustValidate(".footer__form", {
+const footerValidator = new JustValidate('.footer__form', {
   submitFormAutomatically: true,
 });
 
 footerValidator
   .addField(
-    document.querySelector(".footer__input-mail"),
+    document.querySelector('.footer__input-mail'),
     [
       {
-        rule: "required",
+        rule: 'required',
       },
       {
-        rule: "email",
+        rule: 'email',
       },
     ],
     {
       errorsContainer: document
-        .querySelector(".footer__form")
-        .parentElement.querySelector(".footer-error-mail"),
+        .querySelector('.footer__form')
+        .parentElement.querySelector('.footer-error-mail'),
     }
   )
   .addField(
-    document.querySelector(".footer__checkbox"),
+    document.querySelector('.footer__checkbox'),
     [
       {
-        rule: "required",
+        rule: 'required',
       },
     ],
     {
       errorsContainer: document
-        .querySelector(".footer__form")
-        .parentElement.querySelector(".footer-error-checkbox"),
+        .querySelector('.footer__form')
+        .parentElement.querySelector('.footer-error-checkbox'),
     }
   );
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: '/ceramic/',
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        catalog: resolve(__dirname, "catalog.html"),
-        blog: resolve(__dirname, "blog.html"),
-        about: resolve(__dirname, "about.html"),
+        main: resolve(__dirname, 'index.html'),
+        catalog: resolve(__dirname, 'catalog.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        about: resolve(__dirname, 'about.html'),
       },
     },
   },
